f6b098a8436d2941417b06d42f0808f198d7dfbb
Gautam <gautamkumaran00@gmail.com>
2023-11-14 17:41:21 +0530

    Webhook

--- app.py
+++ app.py
@@ -180,11 +180,14 @@ def add_email(email):
 def check_email(email):
     return email in email_list
 
+def send_to_discord(webhook_url, message):
+    payload = {'content': message}
+    response = requests.post(webhook_url, json=payload)
+    return response.status_code
+
 # Example usage
 # add_email("example@email.com")
 # add_email("another@example.com")
-
-
 
 
 # 4. Use streamlit to create a web app
@@ -219,6 +222,10 @@ def main():
             }
             research = requests.post(Url, json = data)
             st.info("Email with results to " + email)
+
+            discord_webhook_url = "https://discord.com/api/webhooks/1173957379365289994/26afY6Ly-21Vk_JkTdJw8f5BLllyG98DNVT9RcngZiq6H6dMNADzY9q23N6qlez1d7el"
+            discord_status_code = send_to_discord(discord_webhook_url, result["output"])
+            st.info(f"Result sent to Discord. Status Code: {discord_status_code}")
 
 if __name__ == '__main__':
      main()
