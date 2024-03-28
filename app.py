02c9509d0cd85a7097ab5da4ea681463e873bf57
Gautam <gautamkumaran00@gmail.com>
2024-03-28 21:46:17 +0530

    some error

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 import os
 from dotenv import load_dotenv
 
-from langchain_community.prompts import PromptTemplate
-from langchain_community.agents import initialize_agent, Tool
-from langchain_community.agents import AgentType
-from langchain_community.chat_models import ChatOpenAI
-from langchain_community.prompts import MessagesPlaceholder
-from langchain_community.memory import ConversationSummaryBufferMemory
-from langchain_community.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.chains.summarize import load_summarize_chain
-from langchain_community.tools import BaseTool
+from langchain.prompts import PromptTemplate
+from langchain.agents import initialize_agent, Tool
+from langchain.agents import AgentType
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import MessagesPlaceholder
+from langchain.memory import ConversationSummaryBufferMemory
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.summarize import load_summarize_chain
+from langchain.tools import BaseTool
 from pydantic import BaseModel, Field
 from typing import Type
 from bs4 import BeautifulSoup
 import requests
 import json
-from langchain_community.schema import SystemMessage
+from langchain.schema import SystemMessage
 from fastapi import FastAPI
 import streamlit as st
 import requests
